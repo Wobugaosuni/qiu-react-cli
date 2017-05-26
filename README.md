dad1961b79268bf8235df8e864a59976cf6e9e2b
qiuxia <397401374@qq.com>
2017-05-26 10:54:25 +0800

    cli init

---
+++ README.md
@@ -0,0 +1,34 @@
+## react-redux-app-cli
+A CLI for scaffolding React.js projects.
+
+<br />
+
+## Installation
+`$ npm install -g react-redux-app-cli`
+
+<br />
+
+## Usage
+`$ react init`
+<br />
+
+## Features
+**Javascript**
+- Language: ES6
+- Loader: Babel
+- Framework: React + Redux
+
+**CSS/Preprocessors**
+- Stylus
+- Postcss
+- Normalize.css
+
+**Module loader**
+- Webpack
+
+**Package manage**
+- npm
+
+**Others**
+- Eslint
+
---
+++ command/init.js
@@ -0,0 +1,38 @@
+'use strict'
+const exec = require('child_process').exec
+const co = require('co')
+const prompt = require('co-prompt')
+const chalk = require('chalk')
+
+const templates = require('../templates')
+
+module.exports = () => {
+ co(function *() {
+    // 处理用户输入
+      // let tplName = yield prompt('Template name: ')
+      let projectName = yield prompt('Project name: ')
+      let gitUrl = 'https://github.com/Wobugaosuni/react-redux-app-cli'
+
+    // if (!templates.tpl[tplName]) {
+    //     console.log(chalk.red('\n × Template does not exit!'))
+    //     process.exit()
+    // }
+    // gitUrl = templates.tpl[tplName].url
+    // branch = templates.tpl[tplName].branch
+
+    // git命令，远程拉取项目并自定义项目名
+    let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && rm -rf .git`
+
+    console.log(chalk.white('\n Start generating...'))
+
+    exec(cmdStr, (error, stdout, stderr) => {
+      if (error) {
+        console.log(error)
+        process.exit()
+      }
+      console.log(chalk.green('\n √ Generation completed!'))
+      console.log(`\n cd ${projectName} && npm install \n`)
+      process.exit()
+    })
+  })
+}
